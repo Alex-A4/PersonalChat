a45d903bd36d2e2505eadab904ebfa9596ed0d80
Alex <alex.A4.25@yandex.ru>
2019-03-27 22:37:42 +0300

    Added primitive UI

--- lib/ui/auth_ui/log_in.dart
+++ lib/ui/auth_ui/log_in.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:personal_chat/bloc/authentication/auth.dart';
 
 class LogInScreen extends StatefulWidget {
   LogInScreen({Key key}) : super(key: key);
+
   @override
   _LogInScreenState createState() => _LogInScreenState();
 }
@@ -9,6 +12,31 @@ class LogInScreen extends StatefulWidget {
 class _LogInScreenState extends State<LogInScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Авторизация'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () {
+                BlocProvider.of<AuthenticationBloc>(context)
+                    .dispatch(AuthAuthenticatedEvent());
+              },
+              elevation: 5.0,
+              color: Colors.purpleAccent[400],
+              textColor: Colors.white,
+              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              child: Text('Войти'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/ui/chat_ui/main_screen.dart
+++ lib/ui/chat_ui/main_screen.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:personal_chat/models/chat.dart';
 
 class MainScreen extends StatefulWidget {
   MainScreen({Key key}) : super(key: key);
+
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
+//TODO: wrap listView with streamBuilder to update chats in real time
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Диалоги'),
+      ),
+      body: ListView.builder(
+        itemCount: 25,
+        itemBuilder: (context, index) {
+          return getListItem(null, index);
+        },
+      ),
+    );
+  }
+
+  //Get the item of list
+  //TODO: add getting info about user by hash
+  Widget getListItem(Chat chat, int index) {
+    return ListTile(
+      title: Text('User Name'),
+      subtitle: Text(
+        'The last message that other user past to that user. We need to do something with that',
+        overflow: TextOverflow.ellipsis,
+        maxLines: 1,
+      ),
+
+      //If last message not read so show icon
+      trailing: index % 2 == 0
+          ? Container(
+              width: 1,
+              height: 1,
+            )
+          : Container(
+              decoration: ShapeDecoration(
+                  color: Colors.purpleAccent[400],
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0))),
+              width: 6,
+              height: 6,
+            ),
+    );
   }
 }
