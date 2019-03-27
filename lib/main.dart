d843aff38c5be6cd8b50fd36765ca9184936c7b8
Alex <alex.A4.25@yandex.ru>
2019-03-27 21:20:59 +0300

    Added dynamic theme widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:personal_chat/ui/app.dart';
-
+import 'package:dynamic_theme/dynamic_theme.dart';
 
 void main() {
-  runApp(MaterialApp(
-    theme: theme,
-    home: App(),
+  runApp(DynamicTheme(
+    defaultBrightness: Brightness.light,
+    data: (brightness) => ThemeData(
+      primaryColor: Colors.purpleAccent[400],
+      brightness: brightness,
+    ),
+    themedWidgetBuilder: (context, theme) {
+      return MaterialApp(
+        theme: theme,
+        home: App(),
+      );
+    },
   ));
 }
-
-final theme = ThemeData(
-  primaryColor: Colors.purpleAccent[400],
-);
