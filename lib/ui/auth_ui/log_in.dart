7f0a4420407d1c2e818753f224617b61ad625adf
Alex <alex.A4.25@yandex.ru>
2019-03-27 10:34:29 +0300

    Init screen classes

---
+++ lib/ui/auth_ui/log_in.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatefulWidget {
+  LogInScreen({Key key}) : super(key: key);
+  @override
+  _LogInScreenState createState() => _LogInScreenState();
+}
+
+class _LogInScreenState extends State<LogInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/ui/auth_ui/sign_in.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  SignInScreen({Key key}) : super(key: key);
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
