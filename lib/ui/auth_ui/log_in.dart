05e08f5f16c13962546ed23fa7544b015f2c2e2d
Alex <alex.A4.25@yandex.ru>
2019-03-28 22:28:57 +0300

    Added minimal logic to login

--- lib/ui/auth_ui/log_in.dart
+++ lib/ui/auth_ui/log_in.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:personal_chat/bloc/authentication/auth.dart';
+import 'package:personal_chat/repositories/user_repository.dart';
+import 'package:personal_chat/ui/auth_ui/sms_dialog.dart';
 
 class LogInScreen extends StatefulWidget {
   LogInScreen({Key key}) : super(key: key);
@@ -12,7 +15,6 @@ class LogInScreen extends StatefulWidget {
 class _LogInScreenState extends State<LogInScreen> {
   TextEditingController _phoneController = TextEditingController();
 
-
   @override
   void dispose() {
     _phoneController.dispose();
@@ -21,6 +23,24 @@ class _LogInScreenState extends State<LogInScreen> {
 
   @override
   Widget build(BuildContext context) {
+    //Callback that notify when user authenticated
+    UserAuthenticated authenticated = (bool auth) {
+      if (!auth)
+        Fluttertoast.showToast(msg: 'Неверный SMS код');
+      else
+        BlocProvider.of<AuthenticationBloc>(context)
+            .dispatch(AuthAuthenticatedEvent());
+    };
+
+    //Callback to show dialog to confirm SMS code
+    ShowSmsCheckDialog dialog = (ConfirmSmsFunc func) {
+      showDialog(
+          context: context,
+          builder: (context) => SmsVerifyDialog(
+                smsFunc: func,
+              ));
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Авторизация'),
@@ -30,24 +50,41 @@ class _LogInScreenState extends State<LogInScreen> {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            TextField(
-              controller: _phoneController,
-              maxLines: 1,
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(hintText: '+7 777 666 33 22'),
+            Container(
+              width: 170.0,
+              child: TextField(
+                textAlign: TextAlign.center,
+                controller: _phoneController,
+                maxLines: 1,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(hintText: '+7 777 666 33 22'),
+                onChanged: (newText) {
+                  if (!_phoneController.text.contains('\+'))
+                    _phoneController.text = '+${newText}';
+                },
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
             ),
             RaisedButton(
               onPressed: () {
                 BlocProvider.of<AuthenticationBloc>(context)
-                    .dispatch(AuthAuthenticatedEvent());
+                    .userRepository
+                    .logInByPhoneNumber(_phoneController.text, dialog, authenticated)
+                    .catchError(
+                        (err) => Fluttertoast.showToast(msg: err.toString()));
               },
               elevation: 5.0,
               color: Colors.purpleAccent[400],
               textColor: Colors.white,
-              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+              padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10.0)),
-              child: Text('Войти'),
+              child: Text(
+                'Войти',
+                style: TextStyle(fontSize: 18.0),
+              ),
             ),
             SizedBox(height: 30.0),
             FlatButton(
