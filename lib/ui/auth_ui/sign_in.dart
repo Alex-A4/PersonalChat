c0ecaaa6a660110b20a82c9e43042ca5c9c0bc9c
Alex <alex.A4.25@yandex.ru>
2019-03-28 22:29:10 +0300

    Added minimal logic lo signin

--- lib/ui/auth_ui/sign_in.dart
+++ lib/ui/auth_ui/sign_in.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:personal_chat/bloc/authentication/auth.dart';
 import 'package:personal_chat/ui/auth_ui/sms_dialog.dart';
 import '../../repositories/user_repository.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class SignInScreen extends StatefulWidget {
   SignInScreen({Key key}) : super(key: key);
@@ -27,7 +28,7 @@ class _SignInScreenState extends State<SignInScreen> {
     //Callback that notify when user authenticated
     UserAuthenticated authenticated = (bool auth) {
       if (!auth)
-        print('Bad');
+        Fluttertoast.showToast(msg: 'Неверный SMS код');
       else
         BlocProvider.of<AuthenticationBloc>(context)
             .dispatch(AuthAuthenticatedEvent());
@@ -43,7 +44,15 @@ class _SignInScreenState extends State<SignInScreen> {
     };
 
     return Scaffold(
-      appBar: AppBar(title: Text('Регистрация')),
+      appBar: AppBar(
+        title: Text('Регистрация'),
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              BlocProvider.of<AuthenticationBloc>(context)
+                  .dispatch(AuthLogInEvent());
+            }),
+      ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(24.0),
         child: Center(
@@ -54,28 +63,42 @@ class _SignInScreenState extends State<SignInScreen> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text('Номер телефона'),
+                  Text('Номер телефона', style: TextStyle(fontSize: 16.0)),
                   Container(
                     width: 150.0,
                     child: TextField(
+                      keyboardType:
+                          const TextInputType.numberWithOptions(signed: true),
+                      textAlign: TextAlign.center,
                       controller: _phoneController,
                       decoration: InputDecoration(hintText: '+7 777 666 33 22'),
                       maxLines: 1,
+                      onChanged: (newText) {
+                        if (!_phoneController.text.contains('\+'))
+                          _phoneController.text = '+${newText}';
+                      },
                     ),
                   ),
                 ],
               ),
+              SizedBox(
+                height: 10.0,
+              ),
               Text(
                 'На указанный номер телефона будет отправлено SMS для подтверждения',
-                style: TextStyle(color: Colors.grey[600], fontSize: 16.0),
+                style: TextStyle(color: Colors.grey[600], fontSize: 14.0),
+              ),
+              SizedBox(
+                height: 20.0,
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Text('Имя пользователя'),
+                  Text('Имя пользователя', style: TextStyle(fontSize: 16.0)),
                   Container(
                     width: 150.0,
                     child: TextField(
+                      textAlign: TextAlign.center,
                       controller: _nameController,
                       maxLines: 1,
                       decoration: InputDecoration(hintText: 'Фёдоров Василий'),
@@ -83,17 +106,28 @@ class _SignInScreenState extends State<SignInScreen> {
                   ),
                 ],
               ),
+              SizedBox(
+                height: 50.0,
+              ),
               RaisedButton(
+                //TODO: add checking phone with RegExp
                 onPressed: () {
                   BlocProvider.of<AuthenticationBloc>(context)
                       .userRepository
                       .signInByPhoneNumber(_nameController.text,
-                          _phoneController.text, dialog, authenticated);
+                          _phoneController.text, dialog, authenticated)
+                      .catchError(
+                          (err) => Fluttertoast.showToast(msg: err.toString()));
                 },
-                padding: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                color: Colors.purple[400],
+                padding: EdgeInsets.fromLTRB(15, 10, 15, 10),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0)),
+                color: Colors.purpleAccent[400],
                 textColor: Colors.white,
-                child: Text('Зарегистрироваться'),
+                child: Text(
+                  'Зарегистрироваться',
+                  style: TextStyle(fontSize: 18.0),
+                ),
               ),
             ],
           ),
